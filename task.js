ae82b2efc77ee359570e0e3a4609e935910d5b3f
Dillon <18711345569@163.com>
2019-06-22 14:14:18 +0800

    add file

---
+++ task.js
@@ -0,0 +1,13 @@
+var name = 'hello';
+var name2 = name.toUpperCase();
+
+var sentence = 'good afternoon,mr mike.';
+var sentence1 = sentence.replace('g','G');
+var sentence2 = sentence1.replace('a','A');
+var sentence3 = sentence2.replace('m','M');
+var sentence4 = sentence3.replace('mi','Mi');
+
+
+var money = '￥20';
+var number = money.substring(1,3);
+console.log(number);
